b4b9a003695b3fbfc006d2a6365eba8db4981c1c
Jahir124 <jahirmp13@gmail.com>
2025-07-30 16:00:18 -0500

    add DisplayName

--- TallerDisenio2A/src/test/java/com/mycompany/tallerdisenio2a/OperationsTest.java
+++ TallerDisenio2A/src/test/java/com/mycompany/tallerdisenio2a/OperationsTest.java
@@ -5,12 +5,15 @@
 package com.mycompany.tallerdisenio2a;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
  * @author joeybustamante
  */
+   
+@DisplayName("Tests for the Operations class")
 public class OperationsTest {
     
     
